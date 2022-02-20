22045c91d11d902dbcfe2d0543f3a557c38f54d9
Troy <troyswanson1010@gmail.com>
2022-02-20 04:24:34 -0600

    using a tmtableview

--- app/src/main/java/com/example/saywhat/ui/main/GettingStartedFrag.kt
+++ app/src/main/java/com/example/saywhat/ui/main/GettingStartedFrag.kt
@@ -5,15 +5,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.saywhat.R
+import com.example.saywhat.databinding.FragGettingStartedBinding
 
 class GettingStartedFrag : Fragment(R.layout.frag_getting_started) {
     private val gettingStartedVM by lazy { ViewModelProvider(this)[GettingStartedVM::class.java] }
+    lateinit var vb: FragGettingStartedBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-    
-    companion object {
-        fun newInstance() = GettingStartedFrag()
+        vb = FragGettingStartedBinding.bind(view)
+        vb.tmtableview.initialize(
+            recipeGrid = gettingStartedVM.recipeGrid.map { it.map { it.toViewItemRecipe(requireContext()) } },
+            shouldFitItemWidthsInsideTable = true,
+        )
     }
 }
--- app/src/main/java/com/example/saywhat/ui/main/GettingStartedVM.kt
+++ app/src/main/java/com/example/saywhat/ui/main/GettingStartedVM.kt
@@ -1,7 +1,39 @@
 package com.example.saywhat.ui.main
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.example.saywhat.databinding.ItemButtonBinding
+import com.tminus1010.tmcommonkotlin.misc.tmTableView.IHasToViewItemRecipe
+import com.tminus1010.tmcommonkotlin.misc.tmTableView.IViewItemRecipe3
+import com.tminus1010.tmcommonkotlin.misc.tmTableView.ViewItemRecipe3
 
 class GettingStartedVM : ViewModel() {
-    // TODO: Implement the ViewModel
+    val recipeGrid =
+        listOf(
+            listOf(
+                object : IHasToViewItemRecipe {
+                    override fun toViewItemRecipe(context: Context): IViewItemRecipe3 {
+                        return ViewItemRecipe3(context, ItemButtonBinding::inflate) { vb ->
+                            vb.btnItem.text = "Howdy Button"
+                        }
+                    }
+                },
+                object : IHasToViewItemRecipe {
+                    override fun toViewItemRecipe(context: Context): IViewItemRecipe3 {
+                        return ViewItemRecipe3(context, ItemButtonBinding::inflate) { vb ->
+                            vb.btnItem.text = "Howdy Button"
+                        }
+                    }
+                },
+            ),
+            listOf(
+                object : IHasToViewItemRecipe {
+                    override fun toViewItemRecipe(context: Context): IViewItemRecipe3 {
+                        return ViewItemRecipe3(context, ItemButtonBinding::inflate) { vb ->
+                            vb.btnItem.text = "Howdy Button"
+                        }
+                    }
+                },
+            ),
+        )
 }

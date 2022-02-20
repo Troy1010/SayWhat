d2c780fc5f6b9595392925c22f6149465553daaf
Troy <troyswanson1010@gmail.com>
2022-02-20 15:31:57 -0600

    bugfix

--- app/src/main/java/com/example/saywhat/app/AppData.kt
+++ app/src/main/java/com/example/saywhat/app/AppData.kt
@@ -2,7 +2,9 @@ package com.example.saywhat.app
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AppData @Inject constructor() {
     var youTubeID = MutableStateFlow<String?>(null)
 }

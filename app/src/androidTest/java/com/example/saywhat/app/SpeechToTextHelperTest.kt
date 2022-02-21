f0af7d90acc2b800762987855c3b33be7cbe8fd2
Troy <troyswanson1010@gmail.com>
2022-02-21 01:49:13 -0600

    SpeechToTextHelper`refactoring for Flow

--- app/src/androidTest/java/com/example/saywhat/app/SpeechToTextHelperTest.kt
+++ app/src/androidTest/java/com/example/saywhat/app/SpeechToTextHelperTest.kt
@@ -2,6 +2,8 @@ package com.example.saywhat.app
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.saywhat.application
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.rx3.asObservable
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.TimeUnit
@@ -17,8 +19,9 @@ internal class SpeechToTextHelperTest {
             inputStream = application.assets.open("10001-90210-01803.wav"),
             sampleRate = 16000f,
         )
-            .doOnNext { if (it is SpeechToTextHelper.SpeechToTextResult.Chunk) logz("result:$it") }
+            .onEach { if (it is SpeechToTextHelper.SpeechToTextResult.Chunk) logz("result:$it") }
             // # When
+            .asObservable()
             .test()
             .apply { await(30, TimeUnit.SECONDS) }
             // # Then
--- app/src/main/java/com/example/saywhat/app/SpeechToTextHelper.kt
+++ app/src/main/java/com/example/saywhat/app/SpeechToTextHelper.kt
@@ -1,9 +1,13 @@
 package com.example.saywhat.app
 
 import android.app.Application
-import io.reactivex.rxjava3.core.Observable
+import com.example.saywhat.all.extensions.easyEmit
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.subjects.PublishSubject
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.rx3.asFlow
 import org.vosk.Model
 import org.vosk.Recognizer
 import org.vosk.android.RecognitionListener
@@ -26,6 +30,7 @@ class SpeechToTextHelper @Inject constructor(private val application: Applicatio
                 { downstream.onError(it) },
             )
         }
+            .toObservable().asFlow()
     private val micSpeechService = model.map { SpeechService(Recognizer(it, 16000.0f), 16000.0f) }
 
     sealed class SpeechToTextResult {
@@ -33,39 +38,46 @@ class SpeechToTextHelper @Inject constructor(private val application: Applicatio
         data class Chunk(val x: String) : SpeechToTextResult()
     }
 
+    object End
+
     /**
      * [sampleRate] example: 16000f
      * [inputStream] example: application.assets.open("10001-90210-01803.wav")
      */
-    fun speechToText(inputStream: InputStream, sampleRate: Float): Observable<SpeechToTextResult> {
-        return model.map {
-            if (inputStream.skip(44) != 44L) throw IOException("File too short")
-            SpeechStreamService(Recognizer(it, sampleRate), inputStream, sampleRate)
-        }.flatMapObservable {
-            val results = PublishSubject.create<SpeechToTextResult>()
-            it.start(object : RecognitionListener {
-                override fun onPartialResult(hypothesis: String) {
-                    results.onNext(SpeechToTextResult.Partial(hypothesis))
-                }
+    fun speechToText(inputStream: InputStream, sampleRate: Float) =
+        model
+            .map {
+                SpeechStreamService(
+                    Recognizer(it, sampleRate),
+                    inputStream.also { if (it.skip(44) != 44L) throw IOException("File too short") },
+                    sampleRate
+                )
+            }
+            .flatMapMerge {
+                val results = MutableSharedFlow<Any>()
+                it.start(object : RecognitionListener {
+                    override fun onPartialResult(hypothesis: String) {
+                        results.easyEmit(SpeechToTextResult.Partial(hypothesis))
+                    }
 
-                override fun onResult(hypothesis: String) {
-                    results.onNext(SpeechToTextResult.Chunk(hypothesis))
-                }
+                    override fun onResult(hypothesis: String) {
+                        results.easyEmit(SpeechToTextResult.Chunk(hypothesis))
+                    }
 
-                override fun onFinalResult(hypothesis: String) {
-                    results.onNext(SpeechToTextResult.Chunk(hypothesis))
-                    results.onComplete()
-                }
+                    override fun onFinalResult(hypothesis: String) {
+                        results.easyEmit(SpeechToTextResult.Chunk(hypothesis))
+                        results.easyEmit(End)
+                    }
 
-                override fun onError(exception: Exception) {
-                    results.onError(exception)
-                }
+                    override fun onError(exception: Exception) {
+                        throw exception
+                    }
 
-                override fun onTimeout() {
-                    results.onError(TimeoutException())
-                }
-            })
-            results
-        }
-    }
+                    override fun onTimeout() {
+                        throw TimeoutException()
+                    }
+                })
+                results
+            }
+            .takeWhile { it is SpeechToTextResult }.map { it as SpeechToTextResult }
 }
